29509b4eb2ba13e7ae8ab80adf641350606a96a3
MagnusMikkelsen <20538868+MagnusMikkelsen@users.noreply.github.com>
2024-12-03 06:43:05 +0100

    Refactor NugetSearch to have an explicit state

--- NugetSearch/MainWindow.xaml.cs
+++ NugetSearch/MainWindow.xaml.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Reactive;
 using System.Reactive.Linq;
 using System.Runtime.CompilerServices;
-using System.Threading;
 using System.Windows;
 using System.Windows.Input;
 using WpfApp1.Annotations;
@@ -21,87 +21,125 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        DataContext = new ViewModel();
-        
 
-        var konami = this
-            .WhenKeyUp()
-            .Konami()
-            .SelectMany(Observable
-                .Timer(TimeSpan.FromMilliseconds(1000))
-                .Select(_ => false)
-                .StartWith(true));
-
-        konami
-            .Select(x => x ? "Snydekode aktiveret" : "")
-            .ObserveOn(SynchronizationContext.Current!)
-            .Subscribe(x => CallingOrTypingText.Content = x);
-
-        konami
-            .Select(x => x ? Visibility.Visible : Visibility.Hidden)
-            .ObserveOn(SynchronizationContext.Current!)
-            .Subscribe(x => CallingOrTypingText.Visibility = x);
+        DataContext = new ViewModel(this.WhenKeyUp());
     }
 }
 
 public class ViewModel : INotifyPropertyChanged
 {
-    public ViewModel()
+    private enum State
     {
+        Idle,
+        Typing,
+        ToShort,
+        Calling,
+        NoResults,
+        Konami
+    }
+
+
+    public ViewModel(IObservable<KeyEventArgs> whenKeyUp)
+    {
+        #region Konami
+
+        var konami = whenKeyUp
+            .Konami()
+            .SelectMany(Observable
+                .Timer(TimeSpan.FromMilliseconds(2500))
+                .Select(_ => false)
+                .StartWith(true))
+            .StartWith(false);
+
+        #endregion
+
+
+        #region user input
         // User types en search field
         var whenTyping = WhenTyping();
 
         // Wait for user to stop typing
         var input = whenTyping
-            .Throttle(TimeSpan.FromMilliseconds(500))
-            .Select(x => (term: x, longEnough: x.Length >= 3));
+            .Throttle(TimeSpan.FromMilliseconds(500));
 
-        var validInput = input
-            .Where(s => s.longEnough)
+        var longEnough = input
+            .Where(x => x.Length >= 3);
+
+        var validInput = longEnough
             .DistinctUntilChanged();
+        #endregion
 
+        #region call api
         // use search term to get autocomplete
         var results = validInput
-            .Select(s => GetAutocomplete(s.term))
+            .Select(GetAutocomplete)
             .Switch() // cancel search if user starts typing again
             .Publish().RefCount(); // share results observable. If we don't do we would do the web request once for each subscriber
+        #endregion
 
-        var inputOnEmptyResult = results
-            .Where(r => r.Length == 0)
-            .WithLatestFrom(input)
-            .Select(x => x.Second)
-            .DistinctUntilChanged();
+        #region state
+        var toShort = input
+            .Where(x => x.Length < 3);
 
+        var notChanged = longEnough
+            .Buffer(2, 1)
+            .Where(b => b.Distinct().Count() == 1);
+
+        var emptyResult = results
+            .Where(r => r.Length == 0);
+
+        var gotResults = results
+            .Where(r => r.Length > 0);
+
+        var state = Observable.Merge(
+            whenTyping.Select(_ => State.Typing),
+            toShort.Select(_ => State.ToShort),
+            notChanged.Select(_ => State.Idle),
+            validInput.Select(_ => State.Calling),
+            emptyResult.Select(_ => State.NoResults),
+            gotResults.Select(_ => State.Idle))
+            .DistinctUntilChanged()
+            .CombineLatest(konami)
+            .Select(x => x.Second ? State.Konami : x.First);
+        #endregion
+
+        #region update gui
+        // results
         results
+            // clear results when input to short
+            .Merge(toShort
+                .Select(_ => Array.Empty<string>()))
             .Subscribe(r => SearchResult = r);
 
-        // Clear results if next search term to short
-        input
-            .Where(s => !s.longEnough)
-            .Subscribe(_ => SearchResult = []);
-
         // Overlay text
-        Observable
-            .Merge(
-                whenTyping.Select(_ => "Typing..."),
-                validInput.Select(_ => "Calling..."),
-                input.Where(s => !s.longEnough).Select(_ => "Type at least 3 letters"),
-                inputOnEmptyResult.Select(i => $"No match for {i.term}"))
-            .DistinctUntilChanged()
+        state
+            .CombineLatest(validInput.StartWith(""))
+            .Select(s => s.First switch
+            {
+                State.Typing => "Typing...",
+                State.ToShort => "Type at least 3 letters",
+                State.Calling => "Calling...",
+                State.NoResults => $"No match for {s.Second}",
+                State.Konami => "Snydekoder aktiveret",
+                State.Idle => "",
+                _ => ""
+            })
             .Subscribe(t => CallingOrTypingText = t);
 
-        // Overlay visibility
-        Observable
-            .Merge(
-                whenTyping.Select(_ => Visibility.Visible),
-                input.Where(x => x.longEnough).Select(_ => Visibility.Hidden),
-                input.Where(x => !x.longEnough).Select(_ => Visibility.Visible),
-                validInput.Select(_ => Visibility.Visible),
-                results.Select(_ => Visibility.Hidden),
-                inputOnEmptyResult.Select(_ => Visibility.Visible))
-            .Throttle(TimeSpan.FromMilliseconds(10))
-            .DistinctUntilChanged()
-            .Subscribe(v => IsCalling = v);
+        state.Select(x => x switch
+        {
+            State.Idle => Visibility.Hidden,
+            State.Typing => Visibility.Visible,
+            State.ToShort => Visibility.Visible,
+            State.Calling => Visibility.Visible,
+            State.NoResults => Visibility.Visible,
+            State.Konami => Visibility.Visible,
+            _ => Visibility.Hidden
+        })
+        //.Throttle(TimeSpan.FromMilliseconds(10))
+        .DistinctUntilChanged()
+        .Subscribe(v => IsCalling = v);
+        #endregion
     }
 
     private IObservable<string> WhenTyping()
@@ -116,7 +154,7 @@ public class ViewModel : INotifyPropertyChanged
 
     private readonly HttpClient _client = new()
     {
-        BaseAddress = new ("https://api-v2v3search-0.nuget.org")
+        BaseAddress = new("https://api-v2v3search-0.nuget.org")
     };
 
     private IObservable<string[]> GetAutocomplete(string searchTerm)
